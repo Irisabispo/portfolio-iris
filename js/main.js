8af0a7def4fbe8c84da0fa97fad85ec9602d94bf
irisabispo <irisabispo@gmail.com>
2026-06-13 17:35:56 -0400

    feat: adiciona menu hamburguer responsivo para mobile

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".menu-hamburguer");
+const menu = document.querySelector(".navbar");
+
+btn.addEventListener("click", () => {
+    btn.classList.toggle("ativo");
+})
+
+
+
+
